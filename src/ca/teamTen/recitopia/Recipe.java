b1cfcd416da077bf9783ef5e26e76b64a6b46fd6
Zhexin Tang <tangzhexin@hotmail.com>
2013-02-28 16:45:56 -0700

    Add basic methods for Recipe class

--- src/ca/teamTen/recitopia/Recipe.java
+++ src/ca/teamTen/recitopia/Recipe.java
@@ -1,27 +1,49 @@
 package ca.teamTen.recitopia;
 
+import java.util.ArrayList;
+
 
 public class Recipe
 {
-	
-	private String name;
-	private String author;
 
-	public Recipe(String name, String[] ingredients, String instructions,
-			String author)
-	{
-		this.name = name;
+	private String recipeName;
+	private String[] ingredients;
+	private String cookingInstruction;
+	private String author;
+	private boolean isPublished;
+	
+	// Recipe constructor
+	public Recipe(String recipeName, String[] ingredients, String instruction, String author) {
+		this.recipeName = recipeName;
+		this.ingredients = ingredients;
+		this.cookingInstruction = instruction;
 		this.author = author;
 	}
-
-	public String getName()
-	{
-		return name;
+	
+	public String getRecipeName() {
+		return this.recipeName;
 	}
-
-	public String getAuthor()
-	{
-		return author;
+	
+	public String showAuthor() {
+		return this.author;
 	}
+	
+	public String[] showIngredients() {
+		return this.ingredients;
+	}
+	
+	public String showCookingInstructions() {
+		return this.cookingInstruction;
+	}
+	
+	/*
+	public Photo[] getPhotos() {
+		
+	}
+	*/
+	public boolean publishRecipe() {
+		return false;
+	}
+	
 
 }
