458fc6680cd25f3db811ad56c4524baa1f6fef77
Alex Wilson <aewilson@ualberta.ca>
2013-02-28 15:55:50 -0700

    Start implementing Recipe

--- src/ca/teamTen/recitopia/Recipe.java
+++ src/ca/teamTen/recitopia/Recipe.java
@@ -3,5 +3,25 @@ package ca.teamTen.recitopia;
 
 public class Recipe
 {
+	
+	private String name;
+	private String author;
+
+	public Recipe(String name, String[] ingredients, String instructions,
+			String author)
+	{
+		this.name = name;
+		this.author = author;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getAuthor()
+	{
+		return author;
+	}
 
 }
