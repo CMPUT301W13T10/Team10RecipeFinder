f3d208afccb6213777ea800d1dc907e197333376
Alex Wilson <aewilson@ualberta.ca>
2013-03-10 11:16:43 -0600

    Add method for checking equality of Recipes.
    Note: Didn't implement object.equals() because that is too
    complicated for now.

--- src/ca/teamTen/recitopia/Recipe.java
+++ src/ca/teamTen/recitopia/Recipe.java
@@ -1,11 +1,12 @@
 package ca.teamTen.recitopia;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class Recipe
 {
-
 	private String recipeName;
 	private ArrayList<String> ingredients;
 	private String cookingInstruction;
@@ -16,7 +17,11 @@ public class Recipe
 	// Recipe constructor
 	public Recipe(String recipeName, ArrayList<String> ingredients, String instruction, String author) {
 		this.recipeName = recipeName;
-		this.ingredients = ingredients;
+		if (ingredients != null) {
+			this.ingredients = new ArrayList<String>(ingredients);
+		} else {
+			this.ingredients = new ArrayList<String>();
+		}
 		this.cookingInstruction = instruction;
 		this.author = author;
 		this.photos = new ArrayList<Photo>();
@@ -37,7 +42,6 @@ public class Recipe
 	public String showCookingInstructions() {
 		return this.cookingInstruction;
 	}
-	
 
 	public Photo[] getPhotos() {
 		return (Photo[]) this.photos.toArray(new Photo[photos.size()]);
@@ -51,5 +55,27 @@ public class Recipe
 		return false;
 	}
 	
-
+	/**
+	 * Checks for equality of all members.
+	 * @return true if all members are equal
+	 */
+	public boolean equalData(Recipe other) {
+		if (other == null) {
+			return false;
+		}
+		
+		Set<String> myIngredients = new HashSet<String>(ingredients);
+		for (String ingredient: other.ingredients) {
+			if (!myIngredients.contains(ingredient)) {
+				return false;
+			}
+		}
+		
+		return recipeName.equals(other.recipeName)
+				&& cookingInstruction.equals(other.cookingInstruction)
+				&& author.equals(other.author)
+				&& (isPublished == other.isPublished)
+				;
+		// TODO: check photos?
+	}
 }
