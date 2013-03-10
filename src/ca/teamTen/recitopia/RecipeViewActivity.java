be417dd001d39a1ace1c36dc9f3cb2f94b124ddf
Zhexin Tang <tangzhexin@hotmail.com>
2013-03-10 13:43:01 -0600

    Add sharing button function for RecipeViewActivity class

--- src/ca/teamTen/recitopia/RecipeViewActivity.java
+++ src/ca/teamTen/recitopia/RecipeViewActivity.java
@@ -1,22 +1,55 @@
 package ca.teamTen.recitopia;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 
 public class RecipeViewActivity extends Activity {
-
+	
+	private ShareActionProvider mShareActionProvider;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recipe_view);
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_recipe_view, menu);
+		
+		// Locate MenuItem with ShareActionProvider
+		MenuItem item = menu.findItem(R.id.menu_item_share);
+		
+		// Fetch and store ShareActionProvider
+	    mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+	    shareRecipe();
 		return true;
 	}
 
+	
+	// Call to update the share intent
+	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+	private void setShareIntent(Intent shareIntent) {
+	    if (mShareActionProvider != null) {
+	        mShareActionProvider.setShareIntent(shareIntent);
+	    }
+	}
+	public void shareRecipe() {
+
+        Intent sendIntent = new Intent();
+    	sendIntent.setAction(Intent.ACTION_SEND);
+    	sendIntent.putExtra(Intent.EXTRA_TEXT, "Actual text to send");
+    	sendIntent.setType("text/plain");
+    	setShareIntent(sendIntent);
+
+	}
 }
